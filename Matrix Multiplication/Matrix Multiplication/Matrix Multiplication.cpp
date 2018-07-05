68e269db8250a7e1af080c1fb1579878152a7254
ShuvaG <40176407+ShuvaG@users.noreply.github.com>
2018-07-05 17:24:58 -0500

    Before adding Transpose
    Matrix multiplication before adding transpose

--- Matrix Multiplication/Matrix Multiplication/Matrix Multiplication.cpp
+++ Matrix Multiplication/Matrix Multiplication/Matrix Multiplication.cpp
@@ -9,57 +9,100 @@ double matrix_1[N][N];
 double matrix_2[N][N];
 double mult_matrix[N][N];
 double sum = 0;
+double r;
+int row1, col1, row2, col2;
 
-int main()
+void multiply_ijk()
 {
-	const int n = 700;
-	int row1 = 0, col1 = 0, row2 = 0, col2 = 0;
-	//int response;
-	ofstream array("array_jki");
-	ofstream time("time_jki");
-	clock_t start, end;
-	double cpu_time_used;
-
-	//cout << "Enter the dimensions of the first matrix: ";
-	//cin >> row1 >> col1;
-	//cout << "Enter the dimensions of the second matrix: ";
-	//cin >> row2 >> col2;
-
-	//cout << endl << "~Entering value for matrix 1~" << endl;
-	for (int row = 0; row < n; row++) // populates first matrix
-	{
-		for (int col = 0; col < n; col++)
+		ofstream array_ijk("array_ijk");
+		ofstream time_ijk("time_ijk");
+		double cpu_time_used_ijk;
+		clock_t start_ijk, end_ijk;
+		row1 =0;
+		col1 =0;
+		row2 =0;
+		col2 =0;
+		for (int a = 0; a < 14; a++)
 		{
-			//cout << "Enter the value for a" << row + 1 << col + 1 << ": ";
-			//cin >> response;
-			matrix_1[row][col] = 1;
+			row1 += 50;
+			col1 += 50;
+			row2 += 50;
+			col2 += 50;
+			start_ijk = clock();
+			for (int i = 0; i < row1; i++) // multiplies the two matricies
+			{
+				for (int j = 0; j < col2; j++)
+				{
+					sum = 0;
+					for (int k = 0; k < col1; k++)
+					{
+						sum += matrix_1[i][k] * matrix_2[k][j];
+					}
+					mult_matrix[i][j] = sum;
+				}
+			}
+			end_ijk = clock();
+			cpu_time_used_ijk = ((double)(end_ijk - start_ijk)) / CLOCKS_PER_SEC;
+			array_ijk << row1 << " ";
+			time_ijk << cpu_time_used_ijk << " ";
 		}
-	}
-	//cout << endl << "~Entering value for matrix 2~" << endl;
-	for (int row = 0; row < n; row++) // poopulates second matrix
-	{
-		for (int col = 0; col < n; col++)
-		{
-			//cout << "Enter the value for b" << row + 1 << col + 1 << ": ";
-			//cin >> response;
-			matrix_2[row][col] = 2;
-		}
-	}
-	for (int row = 0; row < n; row++) // initilizes every postion with a zero
-	{
-		for (int col = 0; col < n; col++)
-		{
-			mult_matrix[row][col] = 0;
-		}
-	}
-	double r;
+		array_ijk.close();
+		time_ijk.close();
+}
+void multiply_kij()
+{
+	ofstream array_kij("array_kij");
+	ofstream time_kij("time_kij");
+	double cpu_time_used_kij;
+	clock_t start_kij, end_kij;
+	row1 = 0;
+	col1 = 0;
+	row2 = 0;
+	col2 = 0;
 	for (int a = 0; a < 14; a++)
 	{
 		row1 += 50;
 		col1 += 50;
 		row2 += 50;
 		col2 += 50;
-		start = clock();
+		start_kij = clock();
+		for (int k = 0; k < col1; k++) // multiplies the two matricies
+		{
+			for (int i = 0; i < row1; i++)
+			{
+				r = matrix_1[i][k];
+				for (int j = 0; j < col2; j++)
+				{
+					mult_matrix[i][j] += r * matrix_2[k][j];
+				}
+			}
+		}
+		end_kij = clock();
+		cpu_time_used_kij = ((double)(end_kij - start_kij)) / CLOCKS_PER_SEC;
+		array_kij << row1 << " ";
+		time_kij << cpu_time_used_kij << " ";
+	}
+	array_kij.close();
+	time_kij.close();
+}
+void multiply_jki()
+{
+
+	ofstream array_jki("array_jki");
+	ofstream time_jki("time_jki");
+	double cpu_time_used_jki;
+	clock_t start_jki, end_jki;
+	row1 = 0;
+	col1 = 0;
+	row2 = 0;
+	col2 = 0;
+	start_jki = clock();
+	for (int a = 0; a < 14; a++)
+	{
+		row1 += 50;
+		col1 += 50;
+		row2 += 50;
+		col2 += 50;
 		for (int j = 0; j < col2; j++) // multiplies the two matricies
 		{
 			for (int k = 0; k < col1; k++)
@@ -67,16 +110,37 @@ int main()
 				r = matrix_2[k][j];
 				for (int i = 0; i < row1; i++)
 				{
-					mult_matrix[i][j] += r * matrix_1[i][k];
+					mult_matrix[i][j] += matrix_1[i][k] * r;
 				}
 			}
 		}
-		end = clock();
-		cpu_time_used = ((double)(end - start)) / CLOCKS_PER_SEC;
-		array << row1 << " ";
-		time << cpu_time_used << " ";
+		end_jki = clock();
+		cpu_time_used_jki = ((double)(end_jki - start_jki)) / CLOCKS_PER_SEC;
+		array_jki << row1 << " ";
+		time_jki << cpu_time_used_jki << " ";
 	}
+	array_jki.close();
+	time_jki.close();
+}
+int main()
+{
+	const int n = 700;
 	
+	for (int row = 0; row < n; row++) // populates first matrix
+	{
+		for (int col = 0; col < n; col++)
+		{
+
+			matrix_1[row][col] = 1;
+			matrix_2[row][col] = 2;
+			mult_matrix[row][col] = 0;
+
+		}
+	}
+	multiply_ijk();
+	multiply_jki();
+	multiply_kij();
+
 	//cout << "The result of multiplying matrix 1 with matrix 2 is: " << endl;
 	//for (int row = 0; row < row1; row++)
 	//{
@@ -86,9 +150,7 @@ int main()
 	//	}
 	//	cout << endl;
 	//}
-	array.close();
-	time.close();
-
+	
 	system("PAUSE");
 	return 0;
 }
